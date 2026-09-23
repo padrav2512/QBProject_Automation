0e3bc4cb7c413a494a4ac3835049feb18d681ea5
padrav2512 <padrav2512@users.noreply.github.com>
2026-09-23 17:01:39 +0530

    Bump processor build for empty MCQ solutions

--- cms_processor.py
+++ cms_processor.py
@@ -22,7 +22,7 @@ from docx.text.paragraph import Paragraph
 from image_pipeline import process_document_images
 
 
-PROCESSOR_BUILD_ID = "2026.09.23-vml-images-v4"
+PROCESSOR_BUILD_ID = "2026.09.23-empty-solution-v5"
 
 MATH_NS = "http://schemas.openxmlformats.org/officeDocument/2006/math"
 WORD_NS = "http://schemas.openxmlformats.org/wordprocessingml/2006/main"
